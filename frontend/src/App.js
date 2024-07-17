368e73d1eaf35ea2f4c7ba0c14bebf6de42b18fc
hartleydavid <56169730+hartleydavid@users.noreply.github.com>
2024-07-16 18:33:08 -0700

    Frontend pulls data from local API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import axios from 'axios';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+
+  state = {details: [], }
+
+  componentDidMount() {
+    let data;
+    axios.get('http://localhost:8000')
+    .then(res => {
+      data = res.data;
+      this.setState({
+        details: data
+      });
+    })
+    .catch(err => {
+      console.error("Error fetching data:", err);
+    });
+  }
+
+  render(){
+    return (
       <div>
-        <h1>Employee's Department Finder</h1>
+        <header> Data Generated From Django </header>
+        <hr></hr>
+        {this.state.details.map((output, id) => (
+          <div key = {id}>
+            <div> 
+              <h2> {output.employee} </h2>
+              <h2> {output.department} </h2>
+            </div>
+          </div>
+        ))}
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
